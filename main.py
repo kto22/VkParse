af8bd5a126e95f9a68e9905fc4722619592c9e55
kto22 <76905058+kto22@users.noreply.github.com>
2024-01-29 10:47:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import func
 
 token = input("Введите ваш токен: ")
-You = input("Введите ваше имя ")
-user_name = input("Введите имя пользователя, с кем вы вели переписку ")
-user_id = int(input("Введите ID пользователся, с которым вели переписку "))
+You = input("Введите ваше имя: ")
+user_name = input("Введите имя пользователя, с кем вы вели переписку: ")
+user_id = int(input("Введите ID пользователся, с которым вели переписку: "))
 
 func.downloader(token, user_id)
 func.repeat_filt()
